b97abc48632cd4013e187e6ca79105d064a0bf2a
Ayb-A <aaraichi.ayoub@gmail.com>
2023-03-22 17:45:41 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "main.h"
+/*
+* main entry point
+* Description - check the code
+* Return 0
+*/
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
