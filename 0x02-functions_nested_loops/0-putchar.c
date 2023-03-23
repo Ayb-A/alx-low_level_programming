c2bfcad7a0bb43a0eca1f5aca2bcf71a4240b792
Ayb-A <aaraichi.ayoub@gmail.com>
2023-03-22 17:38:58 -0700

    prints alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include "main.h"
-#include<unistd.h>
 /*
 * main entry point
 * Description - print _putchar to stdout
@@ -20,17 +19,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-}
-
-
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
 }
---
+++ 0x02-functions_nested_loops/2-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/*
+* main entry point
+* Description - prints lowercase alphabet to stdout
+* Return 0
+*/
+
+void print_alphabet(void)
+{
+int i;
+for(i=97;i<=122;i++)
+{
+_putchar('%c',i);
+}
+_putchar('\n');
+}
