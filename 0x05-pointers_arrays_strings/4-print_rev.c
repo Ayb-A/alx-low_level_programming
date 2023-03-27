3497f25665bb1f1fe92e1b259408764728a6e2c6
Ayb-A <aaraichi.ayoub@gmail.com>
2023-03-27 10:42:10 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print the str in reverse
+ * @s: the string.
+ * Return: Nothing.
+ */
+void print_rev(char *s)
+{
+int i = 0, len = 0;
+while (s[i] != '\0')
+{
+len++;
+i++;
+}
+
+while (len >= 0)
+{
+_putchar(s[len]);
+len--;
+}
+_putchar('\n');
+
+}
